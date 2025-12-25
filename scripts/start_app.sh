2ba3ca8b5573073bd1d529f9d53c6664790935d4
SyedShakirX <159051009+SyedShakirX@users.noreply.github.com>
2025-12-25 17:13:37 +0530

    Update start_app.sh

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+set -e
 
+#The Image will be pulled from my ECR
+docker pull public.ecr.aws/n2p7k0f2/aws-ci-cd-python-flask:latest
+
+#I have enabled port 8000 on Security Group attached to my EC2
+docker run -p 5000:8000 aws-ci-cd-python-flask:latest
