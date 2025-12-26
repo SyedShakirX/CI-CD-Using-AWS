fade0396e5089521133697e2046a597755b732eb
SyedShakirX <159051009+SyedShakirX@users.noreply.github.com>
2025-12-26 16:34:17 +0530

    Update stop_app.sh

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -e
 
-echo "hiiiiiiiiiiiii"
+RUNNING=$(docker ps -q)
+if [ -n "$RUNNING" ]; then
+  docker stop $RUNNING
+fi
+
+ALL=$(docker ps -aq)
+if [ -n "$ALL" ]; then
+  docker rm $ALL
+else
+  echo "No containers to remove."
+fi
