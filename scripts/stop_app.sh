e12bfaf40e9e6682ac7162d12dd6441adeff7741
SyedShakirX <159051009+SyedShakirX@users.noreply.github.com>
2025-12-26 13:49:44 +0530

    Update stop_app.sh

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 set -e
 
-# Get IDs of all running containers
-contIDs=$(docker ps -q)
-
-if [ -n "$contIDs" ]; then
-  docker rm -f $contIDs
-else
-  echo "No running containers to remove."
-fi
+echo "hiiiiiiiiiiiii"
